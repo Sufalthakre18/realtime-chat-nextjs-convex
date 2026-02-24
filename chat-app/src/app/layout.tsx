98160346369dbabe2fdea804236f4f46b285be51
Sufal Thakre <sufalthakre1@gmail.com>
2026-02-24 08:39:53 +0530

    set up Convex provider and auth pages

--- chat-app/src/app/layout.tsx
+++ chat-app/src/app/layout.tsx
@@ -1,18 +1,20 @@
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvider from "@/components/ConvexClientProvider";
+
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body>
-        <ClerkProvider>
-        {children}
-        </ClerkProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="antialiased bg-slate-50">
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
