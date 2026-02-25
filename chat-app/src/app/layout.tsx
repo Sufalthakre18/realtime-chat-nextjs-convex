011bf0e86bcdc911bc3bb33c19c2557d613602de
Sufal Thakre <sufalthakre1@gmail.com>
2026-02-25 16:00:00 +0530

    fix bugs

--- chat-app/src/app/layout.tsx
+++ chat-app/src/app/layout.tsx
@@ -2,19 +2,19 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClientProvider from "@/components/ConvexClientProvider";
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans, Sora,Manrope } from "next/font/google";
+import { Plus_Jakarta_Sans, Sora } from "next/font/google";
 
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
-  variable: "--font-mono",
-  display:"swap"
+  variable: "--font-jakarta",
+  display: "swap",
 });
 
 const sora = Sora({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
-  variable: "--font-serif",
+  variable: "--font-sora",
 });
 
 export const metadata: Metadata = {
@@ -28,12 +28,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${jakarta.variable}  ${sora.variable} font-(--font-jakarta) `}>
-          <ConvexClientProvider>{children}</ConvexClientProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${jakarta.variable} ${sora.variable}`}>
+        <ClerkProvider>
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
